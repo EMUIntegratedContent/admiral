79ea86e8d110a9f67e7903272b9b7c6f8c703970
Chris Puzzuoli <cpuzzuol@F5KQ9024F9VN.local>
2017-07-13 10:58:11 -0400

    trying to figure out stupid browserify/vueify/stupify

---
+++ src/vueComponents/admin_UserInfo.vue
@@ -0,0 +1,54 @@
+<style>
+</style>
+
+<template>
+  <table class="stack" id="users-table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Level</th>
+        <th>Last Login</th>
+        <th v-show="canDelete">Active</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users">
+        <td><a :href="'user/' + user.google.name + '/edit'">{{user.google.name}}</a></td>
+        <td>{{user.google.email}}</td>
+        <td></td>
+        <td></td>
+        <td v-show="canDelete">
+          <input type="checkbox" v-model="is_active" @click="toggleActive(user._id)" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'AdminUsersTable',
+    data: () => ({
+      is_active: false
+    }),
+    props: ['users', 'canDelete'],
+    mounted() {
+
+    },
+    components: {
+
+    },
+    methods: {
+      // Toggles a user's status from (in)active to (in)active
+      toggleActive(userId){
+        axios.post('/api/user/deactivate', { userId: userId })
+          .then((response) => {
+            console.log(response)
+            this.is_active === true ? this.is_active = false : this.is_active = true
+          })
+      }
+    }
+  }
+</script>
